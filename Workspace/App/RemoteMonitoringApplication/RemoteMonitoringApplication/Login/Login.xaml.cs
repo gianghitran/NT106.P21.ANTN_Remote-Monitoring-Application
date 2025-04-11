628f0fefaff40a7fe27d272eddd66aeb2a75718d
Nghia2123 <23521018@gm.uit.edu.vn>
2025-04-11 20:52:06 +0700

    Demo login

--- Workspace/App/RemoteMonitoringApplication/RemoteMonitoringApplication/Login/Login.xaml.cs
+++ Workspace/App/RemoteMonitoringApplication/RemoteMonitoringApplication/Login/Login.xaml.cs
@@ -31,5 +31,15 @@ namespace RemoteMonitoringApplication.Login
                 this.DragMove();
             }
         }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
