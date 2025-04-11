93b5f174fb8a3a18dcfa369d740edd7793b99590
NghiTran <nghi.forwork05@gmail.com>
2025-04-11 21:35:43 +0700

    update UI server

--- Workspace/App/SERVER_RemoteMonitoring/SERVER_RemoteMonitoring/Server/SERVER.xaml.cs
+++ Workspace/App/SERVER_RemoteMonitoring/SERVER_RemoteMonitoring/Server/SERVER.xaml.cs
@@ -23,5 +23,22 @@ namespace SERVER_RemoteMonitoring.Server
         {
             InitializeComponent();
         }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
