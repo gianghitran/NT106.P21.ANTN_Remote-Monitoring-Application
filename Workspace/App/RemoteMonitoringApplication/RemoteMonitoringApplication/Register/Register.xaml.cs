aeb1a2459a7858bff40cacf01d727ec54a72aeaf
Nghia2123 <23521018@gm.uit.edu.vn>
2025-04-13 07:31:38 +0700

    Complete frontend for login and register

---
+++ Workspace/App/RemoteMonitoringApplication/RemoteMonitoringApplication/Register/Register.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RemoteMonitoringApplication.Register
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void navLogin_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Application.Current.Windows.OfType<Login.Login>().Any())
+            {
+                Application.Current.Windows.OfType<Login.Login>().First().Show();
+            }
+            else
+            {
+                Login.Login loginWindow = new Login.Login();
+                loginWindow.Show();
+                loginWindow.Activate();
+            }
+            this.Hide();
+        }
+    }
+}
